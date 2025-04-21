3ed60fc22185b6ccc74d3efe8987eed5454b7b28
HishamHR5 <hrs8606663468@gmail.com>
2025-04-21 14:43:10 +0530

    partial functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainScrollerComponent } from './components/main-scroller/main-scroller.component';
-import { AngularFireModule } from '@angular/fire/compat';
-
 import {environment} from "../enviroinment"
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +20,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
     AngularFirestoreModule,
     BrowserModule,
     FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
